85d6ee7a12e85e92525468ede81974f0a00ffb0c
Basile Koko <basile_koko@hotmail.com>
2017-05-19 08:50:07 +0100

    refactoring

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
-      redirect_to root_path
+      redirect_to articles_path
     else
       flash.now[:danger] = "The login details are wrong"
       render 'new'
@@ -17,6 +18,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have logged out"
+    redirect_to root_path
   end
 
 end
